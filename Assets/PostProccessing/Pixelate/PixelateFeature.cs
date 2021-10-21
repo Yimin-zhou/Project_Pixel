c8f1eafb9c9c7a6c1facc7095f5404f0d52ab23b
yimin zhou <78299555+Yiminzhou-VU@users.noreply.github.com>
2021-10-21 11:49:07 +0200

    initial push

---
+++ Assets/PostProccessing/Pixelate/PixelateFeature.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class PixelateFeature : ScriptableRendererFeature
+{
+    class PixelateRenderPass : ScriptableRenderPass
+    {
+        private RenderTargetIdentifier source;
+        private RenderTargetHandle tempTexture;
+        private Pixelate pixelateProperties;
+        private FilterMode filterMode;
+        private RenderObjects.FilterSettings filteringSettings;
+
+        public PixelateRenderPass(FilterMode filterMode,RenderObjects.FilterSettings filteringSettings)
+        {
+            this.filterMode = filterMode;
+            this.filteringSettings = filteringSettings;
+            
+            tempTexture.Init("_TempPixelatedTexture");
+        }
+
+        public void SetUp(RenderTargetIdentifier source)
+        {
+            
+            this.source = source;
+        }
+        
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            //找到PixelateVolume
+            var stack = VolumeManager.instance.stack;
+            pixelateProperties = stack.GetComponent<Pixelate>();
+            if (pixelateProperties == null || !pixelateProperties.IsActive()) { return; }
+            
+            CommandBuffer cmd = CommandBufferPool.Get("PixelatedFeature");
+            RenderTextureDescriptor cameraTextureDesc = renderingData.cameraData.cameraTargetDescriptor;
+            int pixelateWidth = cameraTextureDesc.width / pixelateProperties.pixelatePower.value;
+            int pixelateHeight = cameraTextureDesc.height / pixelateProperties.pixelatePower.value;
+            cmd.GetTemporaryRT(tempTexture.id,pixelateWidth,pixelateHeight,0,filterMode);
+            cmd.Blit(source,tempTexture.Identifier());
+            cmd.Blit(tempTexture.Identifier(),source);
+            
+            //执行和释放
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);       
+        }
+        
+        public override void FrameCleanup(CommandBuffer cmd)
+        {
+            cmd.ReleaseTemporaryRT(tempTexture.id);
+        }
+    }
+
+    public RenderPassEvent passEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+    public FilterMode filterMode = FilterMode.Point;
+    public RenderObjects.FilterSettings filterSettings;
+    private PixelateRenderPass m_ScriptablePass;
+
+    public override void Create()
+    {
+        m_ScriptablePass = new PixelateRenderPass(filterMode,filterSettings);
+        m_ScriptablePass.renderPassEvent = passEvent;
+    }
+    
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        m_ScriptablePass.SetUp(renderer.cameraColorTarget);
+        renderer.EnqueuePass(m_ScriptablePass);
+    }
+}
+
+
---
+++ Assets/PostProccessing/Pixelate/PixelateVolume.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UnityEngine.Rendering.Universal
+{
+    [Serializable, VolumeComponentMenu("Post-processing/Pixelate")]
+    public sealed class Pixelate : VolumeComponent, IPostProcessComponent
+    {
+        [Tooltip("Filters out pixels under this level of brightness. Value is in gamma-space.")]
+        public ClampedIntParameter pixelatePower = new ClampedIntParameter(5,1,124);
+        public bool IsActive() => pixelatePower.value > 0f;
+
+        public bool IsTileCompatible() => false;
+    }
+}
